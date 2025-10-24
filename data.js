291ff90a27ee1a873423f186103d0c9977d62f98
desmond.chong <desmond.chong@beans.com.my>
2025-10-24 09:30:54 +0800

    Setup the data

---
+++ data.js
@@ -0,0 +1,23 @@
+const introData={
+    date:"JULY 23, 2022",
+    topic:"My new journey as a bootcamp student.",
+    content: "After several months of learning in the Frontend Developer Career Path, I've made the big jump over to the Bootcamp to get expert code reviews of my Solo Projects projects and meet like-minded peers."
+}
+
+const blogData=[
+   {
+        date:"JULY 23, 2022",
+        topic:"My new journey as a bootcamp student.",
+        content: "After several months of learning in the Frontend Developer Career Path, I've made the big jump over to the Bootcamp to get expert code reviews of my Solo Projects projects and meet like-minded peers."
+    },
+    {
+        date:"JULY 23, 2022",
+        topic:"Blog two",
+        content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
+    },
+       {
+        date:"JULY 23, 2022",
+        topic:"Blog three",
+        content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
+    }
+]
